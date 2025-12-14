d18585702f7e6c04d971bae0faf94036c3191efa
Ivan Mitrofanov <aaaaaagrrrrrr@gmail.com>
2025-12-14 04:48:53 +0300

    Remove custom devices management + add FAQ button

--- config/settings.py
+++ config/settings.py
@@ -6,11 +6,7 @@ from typing import Optional, List, Dict, Any
 
 class Settings(BaseSettings):
     PANEL_FIND_USER_BY_USERNAME_PATH: str = '/users?username={username}'
-    DEVICES_MANAGEMENT_ENABLED: bool = False
-    DEVICES_API_MODE: str = 'HWID'
     PANEL_FIND_USER_BY_TG_PATH: str = '/users?telegram_id={tg_id}'
-    PANEL_DEVICES_LIST_PATH: str = '/hwid/devices/{user_uuid}'
-    PANEL_DEVICE_DELETE_PATH: str = '/hwid/devices/delete'
     LANGUAGE_SWITCH_ENABLED: bool = True
     CHANNEL_LINK: str | None = None
     BOT_TOKEN: str
