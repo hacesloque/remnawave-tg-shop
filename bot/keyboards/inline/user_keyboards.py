4bdc3da30a39b659e7c5e4733c967ba80859a60d
Ivan Mitrofanov <ivan@example.com>
2026-01-06 13:35:57 +0000

    ui: remove language button, add FAQ button

--- bot/keyboards/inline/user_keyboards.py
+++ bot/keyboards/inline/user_keyboards.py
@@ -36,19 +36,30 @@ def get_main_menu_inline_keyboard(
         callback_data="main_action:apply_promo")
     builder.row(referral_button, promo_button)
 
-    language_button = InlineKeyboardButton(
-        text=_(key="menu_language_settings_inline"),
-        callback_data="main_action:language")
+    builder.row(
+    InlineKeyboardButton(
+        text=_(key="menu_faq_button"),
+        url="https://static.netaway.top/faq/index.html"
+        )
+    )
+
+
+    # language_button = InlineKeyboardButton(
+
+    #     text=_(key="menu_language_settings_inline"),
+
+    #     callback_data="main_action:language")
     status_button_list = []
     if settings.SERVER_STATUS_URL:
         status_button_list.append(
             InlineKeyboardButton(text=_(key="menu_server_status_button"),
                                  url=settings.SERVER_STATUS_URL))
 
-    if status_button_list:
-        builder.row(language_button, *status_button_list)
-    else:
-        builder.row(language_button)
+    # if status_button_list:
+
+    #     builder.row(language_button, *status_button_list)
+    # else:
+    #     builder.row(language_button)
 
     if settings.SUPPORT_LINK:
         builder.row(
